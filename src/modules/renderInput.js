c6dde30037975586d86e46a8280582b2ce705642
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-07-31 15:01:09 +0500

    Add html markup with styling

--- src/modules/renderInput.js
+++ src/modules/renderInput.js
@@ -1,26 +1,3 @@
-const renderInput = () => {
-  const parent = document.body;
-
-  const h1 = document.createElement('h1');
-  h1.textContent = 'Leaderboard';
-  parent.insertBefore(h1, parent.children[0]);
-
-  const mainDiv = document.createElement('div');
-  mainDiv.id = 'main-div';
-  mainDiv.className = 'main-div';
-
-  const userInputDiv = document.createElement('div');
-  userInputDiv.id = 'user-input-div';
-  userInputDiv.className = 'user-input-div';
-
-  const leaderboardDiv = document.createElement('div');
-  leaderboardDiv.id = 'leaderboard-div';
-  leaderboardDiv.className = 'leaderboard-div';
-
-  mainDiv.appendChild(userInputDiv);
-  mainDiv.appendChild(leaderboardDiv);
-
-  parent.insertBefore(mainDiv, parent.children[1]);
-};
+const renderInput = () => {};
 
 export default renderInput;
