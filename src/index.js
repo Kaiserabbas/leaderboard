ddb3a0e616ecab179cce2e9e4678b7a354bc6ab3
Qaisar <kayser.abbas@gmail.com>
2023-08-02 15:05:34 +0500

    Add functions to fetch data from API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import './modules/style.css';
-import { renderScores, handleSubmit } from './modules/renderScores.js';
+import { getScores, displayScores, saveScore } from './modules/renderScores';
 
 const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const username = document.getElementById('username').value;
+  const score = document.getElementById('yourscore').value;
 
-// Initial render of the scoreboard
-renderScores();
+  if (!username || !score) {
+    console.error('Please fill in both username and score fields.');
+    return;
+  }
+
+  await saveScore(username, score);
+  // After submitting, refresh the scores to display the updated list
+  const scores = await getScores();
+  displayScores(scores);
+  // Reset the form fields after submission
+  form.reset();
+});
+
+// On page load, fetch and display the initial scores
+window.addEventListener('DOMContentLoaded', async () => {
+  const scores = await getScores();
+  displayScores(scores);
+});
 
 const refreshButton = document.getElementById('refresh-btn');
-refreshButton.addEventListener('click', () => {
-  location.reload();
+refreshButton.addEventListener('click', async () => {
+  const scores = await getScores();
+  displayScores(scores);
 });
