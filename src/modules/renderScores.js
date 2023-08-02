f22df42827a5b118e98d418309a07498f5616533
Qaisar <kayser.abbas@gmail.com>
2023-08-02 15:07:39 +0500

    Add code for rendering scoreboard and user inputs

--- src/modules/renderScores.js
+++ src/modules/renderScores.js
@@ -1,46 +1,56 @@
-import addScore from './addScore.js';
-import { getScores, initializeLocalStorage } from './localStorage.js';
-// Initialize local storage with pre-loaded data (if not already initialized)
-if (!localStorage.getItem('scores')) {
-  initializeLocalStorage();
-}
-// Function to render the scoreboard on the page
-const renderScores = () => {
-  const scoreboardDiv = document.getElementById('scoreboard');
-  scoreboardDiv.innerHTML = '';
+import urlAPI from './api';
 
-  const scores = getScores();
+const getScores = async () => {
+  try {
+    const response = await fetch(urlAPI);
+    const data = await response.json();
+    return data.result;
+  } catch (error) {
+    console.error('Error fetching scores:', error);
+  }
+};
 
-  scores.forEach((score) => {
-    const scoreElement = document.createElement('div');
-    scoreElement.className = 'score-element';
-    scoreElement.textContent = `${score.username}: ${score.score}`;
-    scoreElement.classList.add(
-      score.index % 2 === 0 ? 'score-even' : 'score-odd'
-    );
-    scoreboardDiv.appendChild(scoreElement);
+const displayScores = (scores) => {
+  const scoreboard = document.getElementById('scoreboard');
+  scoreboard.innerHTML = '';
+
+  scores.forEach((score, index) => {
+    const scoreDiv = document.createElement('div');
+    scoreDiv.innerText = `${score.user} - ${score.score}`;
+    scoreDiv.classList.add('score-item');
+    if (index % 2 === 0) {
+      scoreDiv.classList.add('white-bg');
+    } else {
+      scoreDiv.classList.add('grey-bg');
+    }
+    scoreboard.appendChild(scoreDiv);
   });
 };
 
-// Function to handle form submission
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const usernameInput = document.getElementById('username');
-  const scoreInput = document.getElementById('yourscore');
+const refreshButton = document.getElementById('refresh-btn');
+refreshButton.addEventListener('click', async () => {
+  const scores = await getScores();
+  displayScores(scores);
+});
 
-  const username = usernameInput.value;
-  const score = scoreInput.value;
-
-  if (username.trim() === '' || score.trim() === '') {
-    alert('Please enter both a username and a score.');
-    return;
+const saveScore = async (user, score) => {
+  try {
+    const payload = {
+      user: user,
+      score: score,
+    };
+    const response = await fetch(urlAPI, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
+    const data = await response.json();
+    console.log(data.result);
+  } catch (error) {
+    console.error('Error saving score:', error);
   }
-  addScore(username, score);
-  renderScores();
-
-  // Clear the form inputs
-  usernameInput.value = '';
-  scoreInput.value = '';
 };
 
-export { renderScores, handleSubmit };
+export { getScores, displayScores, saveScore };
